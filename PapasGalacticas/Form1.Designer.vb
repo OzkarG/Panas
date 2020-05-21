7ff67daeee9770cf9d8075514c0226c4039f636c
miriaampriscilaa@hotmail.com <miriaampriscilaa@hotmail.com>
2020-05-21 14:55:03 -0500

    AQUÍ ESTÁ EL PRIMERO

--- PapasGalacticas/Form1.Designer.vb
+++ PapasGalacticas/Form1.Designer.vb
@@ -23,6 +23,7 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
         Me.SuspendLayout()
         '
         'Label1
@@ -35,11 +36,22 @@ Partial Class Form1
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "SOBRES, cuajate de creatividad"
         '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(162, 74)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(37, 13)
+        Me.LinkLabel1.TabIndex = 1
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "HERE"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LinkLabel1)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -49,4 +61,5 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
 End Class
--- PapasGalacticas/Form1.vb
+++ PapasGalacticas/Form1.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Form1
-
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("www.youtube.com/watch?v=Lt0Hnm_h32Q")
+    End Sub
 End Class
